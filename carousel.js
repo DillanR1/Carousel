243fde6b30c5af3a69f54b6d421d1263d1dc55bb
DillanR1 <dillanroby@yahoo.com>
2022-02-11 23:43:19 -0800

    pics added, js started

---
+++ carousel.js
@@ -0,0 +1,6 @@
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel__button--right");
+const prevButton = document.querySelector(".carousel__button--left");
+const dotNav = document.querySelector(".carousel__nav");
+const dot = Array.from(dotNav.children);
