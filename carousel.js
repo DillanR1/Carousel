e02ec763d557a2d290ffd15c15743a3c2eaa60ca
DillanR1 <dillanroby@yahoo.com>
2022-02-12 23:56:14 -0800

    carousel slider functionality added

--- carousel.js
+++ carousel.js
@@ -4,3 +4,25 @@ const nextButton = document.querySelector(".carousel__button--right");
 const prevButton = document.querySelector(".carousel__button--left");
 const dotNav = document.querySelector(".carousel__nav");
 const dot = Array.from(dotNav.children);
+
+const slideSize = slides[0].getBoundingClientRect().width;
+
+// arrange the slides next to eachother so they are not on top of eachother
+
+// slides[0].style.left = slideWidth * 0 + "px";
+// slides[1].style.left = slideWidth * 1 + "px";
+// slides[2].style.left = slideWidth * 2 + "px";
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(setSlidePosition);
+
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
+  // move to the next slide
+  track.style.transform = "translateX(" + amountToMove + ")";
+});
